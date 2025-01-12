d1b1eed07edadf6c105ff85fd4d8c944f3840f45
José Leonel Inga Quintana <josingaq.codes@gmail.com>
2025-01-11 20:35:33 -0500

    Update vidstack-player.tsx

--- src/components/vidstack-player.tsx
+++ src/components/vidstack-player.tsx
@@ -2,21 +2,21 @@
 
 import { MediaPlayer, MediaProvider, Track } from "@vidstack/react";
 
-import {
-  PlyrLayout,
-  plyrLayoutIcons,
-} from "@vidstack/react/player/layouts/plyr";
-
-import "@vidstack/react/player/styles/base.css";
-import "@vidstack/react/player/styles/plyr/theme.css";
-
 // import {
-//   defaultLayoutIcons,
-//   DefaultVideoLayout,
-// } from "@vidstack/react/player/layouts/default";
+//   PlyrLayout,
+//   plyrLayoutIcons,
+// } from "@vidstack/react/player/layouts/plyr";
 
-// import "@vidstack/react/player/styles/default/theme.css";
-// import "@vidstack/react/player/styles/default/layouts/video.css";
+// import "@vidstack/react/player/styles/base.css";
+// import "@vidstack/react/player/styles/plyr/theme.css";
+
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
+
+import "@vidstack/react/player/styles/default/theme.css";
+import "@vidstack/react/player/styles/default/layouts/video.css";
 
 export const VidstackPlayer = () => {
   return (
@@ -31,8 +31,8 @@ export const VidstackPlayer = () => {
           src="https://ycdcgnsocucu.acek-cdn.com/vtt/01/02905/6a5bioyk9u3j_spa.vtt"
         />
       </MediaProvider>
-      <PlyrLayout icons={plyrLayoutIcons} />
-      {/* <DefaultVideoLayout icons={defaultLayoutIcons} /> */}
+      {/* <PlyrLayout icons={plyrLayoutIcons} /> */}
+      <DefaultVideoLayout icons={defaultLayoutIcons} />
     </MediaPlayer>
   );
 };
